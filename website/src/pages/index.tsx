7153d76a8d241e9d819ba1a7852ee7c8d8b9e00f
Liviu Ionescu <ilg@livius.net>
2024-10-15 03:34:16 +0300

    website: updates

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -42,7 +42,7 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout 
-      title={`Welcome to realpath`}
+      title="Welcome!"
       description="A binary package with the realpath executables">
       <HomepageHeader />
       <main>
